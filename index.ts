8ef1eaf2d045cb4a5cb25269be89f94f346d1754
luoni-krnl <luoni@krnl.xyz>
2024-05-31 05:52:15 -0400

    feat: updated script for sendTransaction

--- index.ts
+++ index.ts
@@ -1,16 +1,52 @@
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 (async () => {
   const token = process.env.ACCESS_TOKEN;
-  const krnlValidator = process.env.KRNL_VALIDATOR as string;
+  const krnlEndpoint = process.env.KRNL_ENDPOINT as string;
+  const krnlWsEndpoint = process.env.KRNL_WS_ENDPOINT as string;
 
-  const connection = new Connection(krnlValidator, "confirmed", token);
+  const connection = new Connection(
+    krnlEndpoint,
+    { commitment: "confirmed", wsEndpoint: krnlWsEndpoint },
+    token
+  );
 
   const faasRequests: string[] = ["KYT", "KYC"];
 
   const hashAndSig = await connection.sendKrnlTransactionRequest(faasRequests);
 
   console.log({ hashAndSig });
+
+  const fromKeypair = Keypair.generate();
+  const toKeypair = Keypair.generate();
+
+  const airdropSignature = await connection.requestAirdrop(
+    fromKeypair.publicKey,
+    LAMPORTS_PER_SOL
+  );
+
+  await connection.confirmTransaction(airdropSignature);
+
+  const lamportsToSend = 1_000_000;
+
+  const transferTransaction = new Transaction({ messages: faasRequests }).add(
+    SystemProgram.transfer({
+      fromPubkey: fromKeypair.publicKey,
+      toPubkey: toKeypair.publicKey,
+      lamports: lamportsToSend,
+    })
+  );
+
+  await sendAndConfirmTransaction(connection, transferTransaction, [
+    fromKeypair,
+  ]);
 })();
